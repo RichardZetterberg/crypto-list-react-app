d733b98500a84b3045315f79a30ac20f37b7c55c
Dmytro <d.dmitrencko2017@gmail.com>
2022-03-17 17:26:10 +0200

    add scroll to top

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,40 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Row, Col } from 'antd';
 import { Link } from 'react-router-dom'
+// import { animateScroll as scroll } from "react-scroll";
+import FooterRights from '../../features/FooterRights';
 import './Footer.css'
 
 const Footer = () => {
-  return (
-    <Row justify='space-around' className='footer'>
-        <Col span={8}>
-            <Row justify='center'>
-                <Link to='/'>Home</Link>
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    };
+
+    return (
+        <Fragment>
+            <Row justify='space-around' className='footer'>
+                <Col span={8}>
+                    <Row justify='center'>
+                        {/* {scrollToTop('/', 'Home')} */}
+                        <Link to='/' onClick={scrollToTop}>Home</Link>
+                    </Row>
+                </Col>
+                <Col span={8}>
+                    <Row justify='center'>
+                        {/* {scrollToTop('/cryptos', 'Cryptos')} */}
+                        <Link to='/cryptos'onClick={scrollToTop}> Cryptos </Link>
+                    </Row>
+                </Col>
+                <Col span={8}>
+                    <Row justify='center'>
+                        {/* {scrollToTop('/news', 'News')} */}
+                        <Link to='/news' onClick={scrollToTop}>News</Link>
+                    </Row>
+                </Col>
             </Row>
-        </Col>
-        <Col span={8}>
-            <Row justify='center'>
-                <Link to='/cryptos'> Cryptos </Link>
-            </Row>
-        </Col>
-        <Col span={8}>
-            <Row justify='center'>
-                <Link to='/news'>News</Link>
-            </Row>
-        </Col>
-    </Row>
-  )
+            <FooterRights />
+        </Fragment>
+    )
 }
 
 export default Footer
---
+++ src/features/FooterRights.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Row, Col } from "antd";
+import "./FooterRights.css";
+
+function FooterRights() {
+  return (
+    <Row className="footer-rights" align="middle" justify="center">
+      <Col span={24}>
+          <Row justify="center">
+            <span>© 2022 CRYPTO-INFO.NET</span>
+          </Row>
+      </Col>
+      <Col span={24}>
+          <Row justify="center">
+            <span>All Rights Reserved © 2022 CRYPTO-INFO.NET</span>
+          </Row>
+      </Col>
+    </Row>
+  );
+}
+
+export default FooterRights;
