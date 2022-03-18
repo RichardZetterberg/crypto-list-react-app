f2c8da8bdbf7712b553db2d1029c8b462738bbe8
Dmytro <d.dmitrencko2017@gmail.com>
2022-03-18 18:59:04 +0200

    add navbar collapse animation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import { Row, Typography, Avatar, Menu, Image } from 'antd'
+import React, { useState } from 'react'
+import { Row, Col, Typography, Avatar, Menu, Button } from 'antd'
 import { Link } from 'react-router-dom'
-import { HomeOutlined, DollarCircleOutlined, BulbOutlined } from '@ant-design/icons';
+import { HomeOutlined, DollarCircleOutlined, BulbOutlined, MenuOutlined } from '@ant-design/icons';
 import navbarLogo from '../../static/navbar-logo.svg'
 import "./Navbar.css"
 
 const Navbar = () => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed)
+    };
+    
   return (
-    <div className='navbar'>
-        <Row justify='space-around'>
-            <Typography.Title level={2} className="logo">
-                <Link to="/">CryptoInfo</Link>
-            </Typography.Title>
-            <Avatar src={navbarLogo} style={{marginTop:'8px'}}/>
+    <div className={collapsed ? 'navbar' : 'navbar-full'}>
+        <Row justify='end'>
+            <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 20 }}>
+                <MenuOutlined />
+            </Button>
         </Row>
-        <Menu theme="dark" style={{backgroundColor:'var(--text-primary)'}}>
+
+        <Menu
+            theme="dark" 
+            style={{backgroundColor:'var(--text-primary)'}}
+            inlineCollapsed={collapsed}
+            mode="inline"
+        >
+            {!collapsed ? (
+                <Row justify='space-between'>
+                    <Col span={18}>
+                        <Row justify='end'>
+                            <Typography.Title level={2} className="logo">
+                                <Link to="/">CryptoInfo</Link>
+                            </Typography.Title>
+                        </Row>
+                    </Col>
+                    <Col span={5}>
+                        <Avatar src={navbarLogo} style={{marginTop:'8px'}}/>
+                    </Col>
+                </Row>
+                ) : (
+                    <Row justify='center'>
+                        <Avatar src={navbarLogo} style={{margin:'8px 0 8px 0'}}/>
+                    </Row>
+                )
+            }
+            
             <Menu.Item key='1' icon={<HomeOutlined />}>
                 <Link to='/'>Home</Link>
             </Menu.Item>
