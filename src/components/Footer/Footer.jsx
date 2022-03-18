262945ebe205e750aafe182546e82ce60efa8108
Dmytro <d.dmitrencko2017@gmail.com>
2022-03-18 15:36:48 +0200

    add scroll to top on footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import { Row, Col } from 'antd';
 import { Link } from 'react-router-dom'
-// import { animateScroll as scroll } from "react-scroll";
+import { animateScroll as scroll } from "react-scroll";
 import FooterRights from '../../features/FooterRights';
 import './Footer.css'
 
@@ -15,19 +15,16 @@ const Footer = () => {
             <Row justify='space-around' className='footer'>
                 <Col span={8}>
                     <Row justify='center'>
-                        {/* {scrollToTop('/', 'Home')} */}
                         <Link to='/' onClick={scrollToTop}>Home</Link>
                     </Row>
                 </Col>
                 <Col span={8}>
                     <Row justify='center'>
-                        {/* {scrollToTop('/cryptos', 'Cryptos')} */}
                         <Link to='/cryptos'onClick={scrollToTop}> Cryptos </Link>
                     </Row>
                 </Col>
                 <Col span={8}>
                     <Row justify='center'>
-                        {/* {scrollToTop('/news', 'News')} */}
                         <Link to='/news' onClick={scrollToTop}>News</Link>
                     </Row>
                 </Col>
